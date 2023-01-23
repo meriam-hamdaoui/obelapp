f38bc3858336997d8d3a4ada35b0742add3c6b5a
meriam <hamdaoui.meriam85@gmail.com>
2023-01-23 11:19:51 +0100

    change header folder to navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./components/navigation/Header";
 
 function App() {
   return (

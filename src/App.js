053bdc2c5ad736763533b781eeb9ff7b547f878d
meriam <hamdaoui.meriam85@gmail.com>
2023-01-23 09:18:40 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello obelass app</div>;
+  return (
+    <div className="App">hello obelass app ************************ test</div>
+  );
 }
 
 export default App;
